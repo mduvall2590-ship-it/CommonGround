235f224707ebde16c5c510489a97919ba010d2d8
mduvall2590-ship-it <mduvall2590@gmail.com>
2026-06-11 23:52:43 +0000

    deleted:    package (1).json 	deleted:    package (2).json 	deleted:    package (3).json 	modified:   package-lock.json 	modified:   package.json 	deleted:    react-router-dom (1).js 	deleted:    react_jsx-dev-runtime (1).js 	deleted:    seed-tn (1).ts 	deleted:    tsconfig (1).json

--- main.tsx
+++ main.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
-import './index.css'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 30000,
-      retry: 1,
-    },
-  },
-})
+function Test() {
+  return <h1 style={{ padding: 20 }}>MOUNT TEST WORKS 🚀</h1>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Test />
   </React.StrictMode>
 )
